9c3fda5167a8427312e4fcdc7f2896b22ab2ea26
Yuval Hager <yhager@yhager.com>
2013-10-07 18:27:24 -0700

    added XdiffExtension

--- hphp/runtime/ext/ext_xdiff.cpp
+++ hphp/runtime/ext/ext_xdiff.cpp
@@ -2,6 +2,14 @@
 
 namespace HPHP {
 
+static class XdiffExtension : public Extension {
+public:
+  XdiffExtension() : Extension("xdiff") { }
+  virtual void moduleLoad(Hdf config) {
+    HHVM_FE(xdiff_native);
+  }
+} s_xdiff_extension;
+
 String HHVM_FUNCTION(xdiff_native) {
   String s("Hello from xdiff cpp");
 
