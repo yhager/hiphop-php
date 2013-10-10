89679e8ceca19a5491f4e534fec49bebf1cd2ab4
Yuval Hager <yhager@yhager.com>
2013-10-09 18:32:47 -0700

    implemented xdiff_string_rabdiff

--- hphp/runtime/ext/ext_xdiff.cpp
+++ hphp/runtime/ext/ext_xdiff.cpp
@@ -1,4 +1,5 @@
 #include "hphp/runtime/ext/ext_xdiff.h"
+#include <xdiff.h>
 
 namespace HPHP {
 
@@ -6,14 +7,63 @@ static class XdiffExtension : public Extension {
 public:
   XdiffExtension() : Extension("xdiff") { }
   virtual void moduleLoad(Hdf config) {
-    HHVM_FE(xdiff_native);
+    HHVM_FE(xdiff_string_rabdiff);
   }
 } s_xdiff_extension;
 
-String HHVM_FUNCTION(xdiff_native) {
-  String s("Hello from xdiff cpp");
 
-  return s;
+static mmfile_t* make_mmfile(const String& str)
+{
+  mmfile_t* mmfile = (mmfile_t*) malloc(sizeof(mmfile_t));
+
+  if (xdl_init_mmfile(mmfile, str.length(), XDL_MMF_ATOMIC) < 0) {
+    free(mmfile);
+    return nullptr;
+  }
+
+  void *mem = xdl_mmfile_writeallocate(mmfile, str.length());
+  if (!mem) {
+    xdl_free_mmfile(mmfile);
+    free(mmfile);
+    return nullptr;
+  }
+
+  memcpy(mem, str.data(), str.length());
+  return mmfile;
+}
+
+static int append_string(void *ptr, mmbuffer_t* bufs, int count) {
+  String* str = (String*) ptr;
+  for (int i=0; i< count; ++i) {
+    str->operator+=(String(bufs[i].ptr, bufs[i].size, CopyString));
+  }
+  return 0;
+}
+
+Variant HHVM_FUNCTION(xdiff_string_rabdiff, const String& old_data,
+                      const String& new_data) {
+	mmfile_t *mmold, *mmnew;
+
+  SCOPE_EXIT {
+    xdl_free_mmfile(mmold);
+    xdl_free_mmfile(mmnew);
+  };
+
+  mmold = make_mmfile(old_data);
+  mmnew = make_mmfile(new_data);
+
+  if (!mmold || !mmnew)
+    return false;
+
+  xdemitcb_t ecb;
+  String output;
+  ecb.priv = &output;
+  ecb.outf = append_string;
+
+	if (xdl_rabdiff(mmold, mmnew, &ecb) < 0)
+    return false;
+
+  return output;
 }
 
 }
--- hphp/system/php/xdiff/xdiff.php
+++ hphp/system/php/xdiff/xdiff.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * xdiff_string_rabdiff() - http://php.net/function.xdiff_string_rabdiff
+ *
+ * @param string $old_data - First string with binary data. It acts as "old" data.
+ * @param string $new_data - Second string with binary data. It acts as "new" data
+ *
+ * @return string - binary diff containing differences between "old" and "new"
+ *                  data or FALSE if an internal error occurred
+ */
 <<__Native>>
-function xdiff_native(): string;
+function xdiff_string_rabdiff(string $old_data, string $new_data): mixed;
 
-function xdiff_php(): mixed {
+function xdiff_php_test(): bool {
   echo "Hello from xdiff_php\n";
   return true;
 }
